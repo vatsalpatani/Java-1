b66c627577d954e43fdf1ee8e023837164e19d0a
Vatsal Patani <100488082+vatsalpatani@users.noreply.github.com>
2023-02-09 21:06:47 +0530

    Update Arithmatic_Operator.java

--- Arithmatic_Operator.java
+++ Arithmatic_Operator.java
@@ -1,38 +1,33 @@
-import java.io.*;
 import java.util.Scanner;
 
 public class Arithmatic_Operator 
 {
         public static void main(String[] args)
         {
-               // Enter First Value Using Scanner
-               System.out.println("Enter First No:");
-               Scanner a = new Scanner(System.in);
-               String b = a.next();
-               int c = Integer.parseInt(b);
-               
-               //Enter Second Value Using Scanner
-               System.out.println("Enter Second No:");
-               Scanner d = new Scanner(System.in);
-               String e = d.next();
-               int f = Integer.parseInt(e); 
-               
-               int add = c + f;
-               System.out.println("Addition :" + add);
-               
-               int sub = c-f;
-               System.out.println("Subtraction :" + sub);
-               
-               int mul = c*f;
-               System.out.println("Multiplication :" + mul);
-               
-               int div = c/f;        
-               System.out.println("Division :" + div);
-               
-               int mod = c%f; 
-               System.out.println("Moduals :" + mod);
-               
-               
-               
+            Scanner a = new Scanner(System.in);
+                    
+            // Enter First Value Using Scanner
+            System.out.println("Enter First No:");
+            int c = a.nextInt();
+            
+            //Enter Second Value Using Scanner
+            System.out.println("Enter Second No:");
+            int f = a.nextInt();
+            
+            int add = c + f;
+            System.out.println("Addition :" + add);
+            
+            int sub = c-f;
+            System.out.println("Subtraction :" + sub);
+            
+            int mul = c*f;
+            System.out.println("Multiplication :" + mul);
+            
+            int div = c/f;        
+            System.out.println("Division :" + div);
+            
+            int mod = c%f; 
+            System.out.println("Moduals :" + mod);
+            a.close();
         }
 }
